e86157b5954608c6bd1e56ef4ce63a91c2ab9c43
bobcao3 <bobcaocheng@163.com>
2015-08-14 17:15:39 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,31 @@
+#!/bin/ruby
+
+require "gtk3"
+
+class MainWindow < Gtk::Window
+
+	def initialize
+		super(:toplevel)
+		
+		@box = Gtk::Box.new(:horizontal, 1)
+		@button_applications = Gtk::Button.new(:label => "Applications", :mnemonic => "Applications", :stock_id => nil)
+		@button_do = Gtk::Button.new(:label => " Do ", :mnemonic => "Do", :stock_id => nil)
+		
+		@button_applications.signal_connect("clicked") {
+			system(GLib.getenv("Launcher"))
+		}
+		
+		@box.pack_start(@button_applications,:expand => false,:fill => true,:padding =>0)
+		@box.pack_start(@button_do,:expand => false,:fill => true,:padding =>0)
+		self.add(@box)
+		
+		screen = Gdk::Screen.default
+		self.resize(screen.width, 32)
+	end
+	
+end
+
+Gtk.init
+win = MainWindow.new
+win.show_all
+Gtk.main
