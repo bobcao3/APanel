2d8ab4e727481e2e81bf9d6e52b3c9b528fc7c9e
bobcao3 <bobcaocheng@163.com>
2015-09-03 14:07:10 +0000

    Add action button

--- main.rb
+++ main.rb
@@ -2,17 +2,45 @@
 
 require "gtk3"
 
+class DoWindow < Gtk::Window
+
+	def initialize
+		super(:popup)
+		self.window_position = Gtk::Window::Position::CENTER_ALWAYS
+		@box = Gtk::Box.new(:vertical, 4)
+		@shutdown = Gtk::Button.new(:label => "Shutdown", :mnemonic => "Shutdown", :stock_id => nil)
+		@shutdown.signal_connect("clicked") {
+			exec("/bin/shutdown","now")
+		}
+		@reboot = Gtk::Button.new(:label => "Reboot", :mnemonic => "Reboot", :stock_id => nil)
+		@reboot.signal_connect("clicked") {
+			exec("/bin/reboot now")
+		}
+		@cancel = Gtk::Button.new(:label => "Cancel", :mnemonic => "Cancel", :stock_id => nil)
+		@cancel.signal_connect("clicked") {
+			self.destroy
+		}
+		@box.pack_start(@shutdown,:expand => true,:fill => true,:padding =>0)
+		@box.pack_start(@reboot,:expand => true,:fill => true,:padding =>0)
+		
+		@box.pack_end(@cancel,:expand => true,:fill => true,:padding =>0)
+		self.add(@box)
+	end
+
+end
+
 class MainWindow < Gtk::Window
 
 	def updateTime
-	
+		t = Time.now
+		@time.label = t.strftime(time_format)
 	end
 
 	def initialize
 		super(:toplevel)
 		self.skip_taskbar_hint = true
 		self.skip_pager_hint = true
-		self.set_type_hint(Gdk::WindowTypeHint::DOCK)
+		#self.set_type_hint(Gdk::WindowTypeHint::DOCK)
 		
 		@box = Gtk::Box.new(:horizontal, 1)
 		@button_applications = Gtk::Button.new(:label => "Applications", :mnemonic => "Applications", :stock_id => nil)
@@ -21,6 +49,11 @@ class MainWindow < Gtk::Window
 		
 		@button_applications.signal_connect("clicked") {
 			system(GLib.getenv("Launcher"))
+		}
+		
+		@button_do.signal_connect("clicked") {
+			dow = DoWindow.new
+			dow.show_all
 		}
 		
 		@box.pack_start(@button_applications,:expand => false,:fill => true,:padding =>0)
